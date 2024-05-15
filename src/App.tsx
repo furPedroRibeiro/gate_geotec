d1cdf2c53a6f69b4153394a6f79c8e22a4b65f7a
furPedroRibeiro <pedrocodejob@gmail.com>
2024-05-15 20:49:45 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,38 @@ import api from './api.tsx'
 
 export function App() {
   const [statusOfGate, setStatusOfGate] = useState<any[]>([])
-  const [status, setStatus] = useState(2)
+  const [showStatus, setShowStatus] = useState(1)
   useEffect(() =>{
-    if(status==2){
+    if(showStatus==1){
       console.log("consumindo api")
       api.get('https://api-gate-geotec.onrender.com/funcionamento').then(res=>{
         setStatusOfGate(res.data.reverse().slice(0,10))
       }).then(()=>{
-        setStatus(3)
+        setShowStatus(0)
       }).catch((err)=>{
         console.error("Ops!! Um erro aconteceu ao consumir api!!!"+err)
       })
     }
+  })
     const dataAtual = new Date();
     console.log(dataAtual.getHours().toString())
     const hourStatus = dataAtual.getHours().toString() + ":" + dataAtual.getMinutes().toString() + " " + dataAtual.getDay().toString() + "/" + dataAtual.getMonth().toString()
-    if(status == 0){
+    function open(){
       api.post('https://api-gate-geotec.onrender.com/funcionamento', {
-        funcionando: status,
+        funcionando: 1,
         horario: hourStatus
-      })
-    } else if(status == 1){
-      api.post('https://api-gate-geotec.onrender.com/funcionamento', {
-        funcionando: status,
-        horario: hourStatus
+      }).then(()=>{
+        setShowStatus(1)
       })
     }
-    api.get('https://api-gate-geotec.onrender.com/funcionamento').then(res=>{
-      setStatusOfGate(res.data.reverse().slice(0,10))
-    }).catch((err)=>{
-      console.error("Ops!! Um erro aconteceu ao consumir api!!!"+err)
-    })
-  })
+    function closed(){
+      api.post('https://api-gate-geotec.onrender.com/funcionamento', {
+        funcionando: 0,
+        horario: hourStatus
+      }).then(()=>{
+        setShowStatus(1)
+      })
+    }
   return (
     <main className='px-12 py-32 gap-4 flex flex-col items-center justify-center md:w-[800px] mx-auto'>
       <h1 className="text-white font-main font-semibold text-2xl flex gap-3 flex-wrap items-center justify-center">Portão da Geotec 
@@ -45,8 +45,8 @@ export function App() {
       <div className="flex flex-col gap-4">
         <p id="title" className="font-main text-emerald-50">O portão está funcionando?</p>
         <div className="flex items-center justify-center gap-3">
-          <button onClick={()=>setStatus(1)} className="py-2 px-3 rounded-md w-20 bg-green-500 text-white font-main font-medium hover:bg-opacity-80 transition-colors duration-700 text-center text-shadow-md shadow-black [text-shadow:_1px_1px_0_rgb(0_0_0_/_50%)]">Sim</button>
-          <button onClick={()=>setStatus(0)} className="py-2 px-3 rounded-md w-20 bg-red-600 text-white font-main font-medium hover:bg-opacity-80 transition-colors duration-700 text-center text-shadow-md shadow-black [text-shadow:_1px_1px_0_rgb(0_0_0_/_50%)]">Não</button>
+          <button onClick={open} className="py-2 px-3 rounded-md w-20 bg-green-500 text-white font-main font-medium hover:bg-opacity-80 transition-colors duration-700 text-center text-shadow-md shadow-black [text-shadow:_1px_1px_0_rgb(0_0_0_/_50%)]">Sim</button>
+          <button onClick={closed} className="py-2 px-3 rounded-md w-20 bg-red-600 text-white font-main font-medium hover:bg-opacity-80 transition-colors duration-700 text-center text-shadow-md shadow-black [text-shadow:_1px_1px_0_rgb(0_0_0_/_50%)]">Não</button>
         </div>
         <div className='m-auto flex flex-col items-center justify-center gap-4'>
           <h2 className='font-main text-white font-semibold text-2xl'>Status</h2>
